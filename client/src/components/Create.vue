94b02095e9c002c592a2b0f2c8c78fa4209d020a
Giovanni <giovi.cestaro@gmail.com>
2022-02-07 11:52:43 +0100

    last edit

--- client/src/components/Create.vue
+++ client/src/components/Create.vue
@@ -1,52 +1,52 @@
 <template>
-<div class="flex flex-col items-center p-10 space-y-7 bg-gray-100">
+<div class="flex flex-col items-left pl-32 pt-16 pb-16 space-y-7 bg-blue-50">
 
-  <div class="flex flex-col w-1/3">
+  <div class="flex flex-col w-2/5">
       <label for="company">Nome del condominio</label>
-      <input id="company" type="text" placeholder="inserire condominio"  class="outline-none border-2 px-2 rounded-lg py-1">
+      <input id="company" type="text" placeholder="inserire condominio"  class="outline-none border-2 px-2 rounded-lg py-1 border-orange-400">
   </div>
 
-  <div class="flex flex-col w-1/3">
+  <div class="flex flex-col w-2/5">
       <label for="foto">Foto </label>
-      <input id="foto" type="text" placeholder="inserire foto" class="outline-none border-2 px-2 rounded-lg py-1">
+      <input id="foto" type="text" placeholder="inserire foto" class="outline-none border-2 px-2 rounded-lg py-1 border-orange-400">
     </div>
 
-  <div class="flex flex-col w-1/3">
+  <div class="flex flex-col w-2/5">
       <label for="company">Comune</label>
-      <input id="company" type="text" placeholder="inserire Comune" class="outline-none border-2 px-2 rounded-lg py-1">
+      <input id="company" type="text" placeholder="inserire Comune" class="outline-none border-2 px-2 rounded-lg py-1 border-orange-400">
   </div>
 
-  <div class="flex flex-col w-1/3">
+  <div class="flex flex-col w-2/5">
       <label for="exp">Provincia</label>
-      <input id="exp" type="text" placeholder="inserire provincia"  class="outline-none border-2 px-2 rounded-lg py-1">
+      <input id="exp" type="text" placeholder="inserire provincia"  class="outline-none border-2 px-2 rounded-lg py-1 border-orange-400">
   </div>
 
-    <div class="flex flex-col w-1/3">
+    <div class="flex flex-col w-2/5">
       <label for="exp">Indirizzo</label>
-      <input id="exp" type="text" placeholder="inserire indirizzo"  class="outline-none border-2 px-2 rounded-lg py-1">
+      <input id="exp" type="text" placeholder="inserire indirizzo"  class="outline-none border-2 px-2 rounded-lg py-1 border-orange-400">
   </div>
 
-  <div class="flex flex-col w-1/3">
+  <div class="flex flex-col w-2/5">
       <label for="exp">CAP</label>
-      <input id="exp" type="text" placeholder="inserire CAP"  class="outline-none border-2 px-2 rounded-lg py-1">
+      <input id="exp" type="text" placeholder="inserire CAP"  class="outline-none border-2 px-2 rounded-lg py-1 border-orange-400">
   </div>
 
-    <div class="flex flex-col w-1/3">
+    <div class="flex flex-col w-2/5">
       <label for="ral">Numero appartamenti</label>
       <input
       id="ral"
       type="number"
       placeholder="inserire numero degli appartamenti"
-      class="outline-none border-2 px-2 rounded-lg py-1">
+      class="outline-none border-2 px-2 rounded-lg py-1 border-orange-400">
   </div>
 
-  <div class="flex flex-col w-1/3">
+  <div class="flex flex-col w-2/5">
       <label for="exp">Consumo complessivo dell'ultimo anno</label>
-      <input id="exp" type="text" placeholder="inserire consumo annuale"  class="outline-none border-2 px-2 rounded-lg py-1">
+      <input id="exp" type="text" placeholder="inserire consumo annuale"  class="outline-none border-2 px-2 rounded-lg py-1 border-orange-400">
   </div>
 
-  <div class=" flex justify-left w-1/3">
-    <button class="bg-orange-700 px-4 py-2 rounded-lg text-white hover:bg-pink-300" @click="create()">Crea scheda</button>
+  <div class=" flex justify-left w-2/5">
+    <button class="bg-orange-700 px-4 py-2 rounded-lg text-white hover:bg-red-600" @click="create()">Crea scheda</button>
   </div>
 
 </div>
@@ -58,7 +58,7 @@ export default {
 
     data (){
         return{
-            terreni: {},
+            house: {},
             isEdit: false
         }
     },
--- client/src/components/Navigation.vue
+++ client/src/components/Navigation.vue
@@ -1,5 +1,6 @@
 <template>
-      <div class="flex p-7 justify-center bg-orange-200 sticky top-0 items-center text-lg">
+      <div class="flex p-4 pl-32 justify-left bg-orange-200 sticky top-0 items-end text-lg">
+          <h1 class="font-bold text-red-700 text-3xl pr-96">ENDI</h1>
           <h1 class="font-bold text-black pr-24">Home</h1>
           <h1 class="font-bold text-black pl-24">List</h1>
       </div>
